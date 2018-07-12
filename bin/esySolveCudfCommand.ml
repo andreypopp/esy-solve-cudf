80886f72a5c87656e336806190c4c1c0e52b7300
Andrey Popp <8mayday@gmail.com>
2018-07-12 12:19:03 +0300

    Write solution into a file, don't fail on no solution

--- bin/esySolveCudfCommand.ml
+++ bin/esySolveCudfCommand.ml
@@ -1,6 +1,9 @@
 module Solver = struct
-  let runWithFilename ?(timeout=5.) ?(strategy="-notuptodate") filename =
-    match Cudf_parser.load_from_file filename with
+  let runWithFilename
+    ?(timeout=5.) ?(strategy="-notuptodate")
+    filenameIn filenameOut
+    =
+    match Cudf_parser.load_from_file filenameIn with
     | exception Cudf_parser.Parse_error (message, (ls, _le)) ->
       let message =
         Printf.sprintf "%s (at line %i)" message ls.Lexing.pos_lnum
@@ -26,10 +29,18 @@ module Solver = struct
 
     match solution with
     | Some solution ->
-      Cudf_printer.pp_solution stdout solution;
+      let oc = open_out filenameOut in
+      let out = IO.output_channel oc in
+      Cudf_printer.pp_io_solution out solution;
+      IO.flush out;
+      IO.close_out out;
+      close_out oc;
       `Ok ()
     | None ->
-      `Error (false, "no solution found")
+      let oc = open_out filenameOut in
+      output_string oc "\n";
+      close_out oc;
+      `Ok ()
 end
 
 module CommandLineInterface = struct
@@ -56,21 +67,29 @@ module CommandLineInterface = struct
       & info ["strategy"; "s"] ~docs ~doc ~docv:"STRATEGY"
     )
 
-  let filename =
+  let filenameIn =
     let doc = "Path to CUDF document file to solve." in
     Arg.(
       required
       & pos 0  (some file) None
-      & info [] ~doc ~docv:"PATH"
+      & info [] ~doc ~docv:"INPUT.CUDF"
+    )
+
+  let filenameOut =
+    let doc = "Path to CUDF solution file to print to generate." in
+    Arg.(
+      required
+      & pos 1  (some string) None
+      & info [] ~doc ~docv:"OUTPUT.CUDF"
     )
 
   let defaultCommand =
     let doc = "Solve CUDF dependency problem" in
     let info = Term.info "esy-solve" ~version ~doc ~sdocs ~exits in
-    let cmd timeout strategy filename =
-      Solver.runWithFilename ?timeout ?strategy filename
+    let cmd timeout strategy filenameIn filenameOut = 
+      Solver.runWithFilename ?timeout ?strategy filenameIn filenameOut
     in
-    Term.(ret (const cmd $ timeout $ strategy $ filename)), info
+    Term.(ret (const cmd $ timeout $ strategy $ filenameIn $ filenameOut)), info
 
   let run () =
     Printexc.record_backtrace true;
